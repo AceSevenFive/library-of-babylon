5d84cbdc8a9f8c1cb2ca884948d3a6727a21d10d
AceSevenFive <pellentesquenonvis@gmail.com>
2016-11-19 10:37:10 -0500

    Various modifications

--- bot.py
+++ bot.py
@@ -1,25 +1,14 @@
 import discord
 import asyncio
+import os.path
 from itertools import chain
 
 client = discord.Client()
 loop = asyncio.get_event_loop()
 varlist = [] * 256
 channels = [] * 256
-nationstats = [] * 256
-chilestats = [] * 10
-polandstats = [] * 10
-constantinoplestats = [] * 10
-chinastats = [] * 10
-kilwastats = [] * 10
-sumerstats = [] * 10
-hellstats = [] * 10
-australistats = [] * 10
-burmastats = [] * 10
-riostats = [] * 10
-nuukstats = [] * 10
-fsastats = [] * 10
 consolechannel = ""
+nations = [] * 256
 @client.event
 
 async def on_ready():
@@ -28,6 +17,8 @@ async def on_ready():
 	print(client.user.id)
 	print('------')
 	updateadmins()
+	updateconsoleoutputchannel()
+	updatenations()
 
 def updateadmins():
 	with open('admin', 'r') as file:
@@ -37,64 +28,13 @@ def updateadmins():
 		author = author.split()
 		varlist.insert(0, author)
 		print('Admins updated successfully.')
-		updatenations()
-
-def updatenations():
-	with open('nations', 'r') as file:
-		global nation
+	with open('nationmods', 'r') as file:
+		global nationmods
 		global varlist
-		nation = file.read()
-		nation = nation.split()
-		varlist.insert(1, nation)
-		print("Nations updated successfully.")
-		updatenationstats()
-		
-def updatenationstats():
-	with open('nationstats', 'r') as file:
-		global nationstats
-		global chilestats
-		global polandstats
-		global constantinoplestats
-		global chinastats
-		global kilwastats
-		global sumerstats
-		global hellstats
-		global australiastats
-		global burmastats
-		global riostats
-		global nuukstats
-		global fsastats
-		global varlist
-		nationstats = file.read()
-		nationstats = nationstats.split()
-		nationstats[0:19] = []
-		varlist.insert(2, nationstats)
-		chilestats = nationstats[0:9]
-		varlist.insert(3, chilestats)
-		polandstats = nationstats[10:19]
-		varlist.insert(4, polandstats)
-		constantinoplestats = nationstats[20:29]
-		varlist.insert(5, constantinoplestats)
-		chinastats = nationstats[30:39]
-		varlist.insert(6, chinastats)
-		kilwastats = nationstats[40:49]
-		varlist.insert(7, kilwastats)
-		sumerstats = nationstats[50:59]
-		varlist.insert(8, sumerstats)
-		hellstats = nationstats[60:69]
-		varlist.insert(9, hellstats)
-		australiastats = nationstats[70:79]
-		varlist.insert(10, australiastats)
-		burmastats = nationstats[80:89]
-		varlist.insert(11, burmastats)
-		riostats = nationstats[90:99]
-		varlist.insert(12, riostats)
-		nuukstats = nationstats[100:109]
-		varlist.insert(13, nuukstats)
-		fsastats = nationstats[110:119]
-		varlist.insert(14, fsastats)
-		print("Nation stats updated successfully.")
-		updateconsoleoutputchannel()
+		nationmods = file.read()
+		nationmods = nationmods.split()
+		varlist.insert(1, nationmods)
+		print("Nation mods updated successfully.")
 		
 def updateconsoleoutputchannel():
 	global consolechannel
@@ -102,13 +42,33 @@ def updateconsoleoutputchannel():
 		consolechannel = file.readlines()
 		consolechannel = " ".join(consolechannel)
 		consolechannel = client.get_channel(consolechannel)
-	varlist.insert(15, consolechannel)
+	varlist.insert(3, consolechannel)
 	print("Console output channel set successfully.")
 	
-async def print_console_channel(content):
+def updatenations():
+	global nations
+	with open('nations', "r") as file:
+		nations = file.read()
+	print("Nations read successfully.")
+	varlist.insert(4, nations)
+	
+async def addnation(name):
+	with open("nations", "a") as file:
+		file.write("\n" + name)
+		updatenations()
+	await print_console_channel("New nation added: " + name, 2)
+	
+async def print_console_channel(content, messagetype):
 	global consolechannel
 	if consolechannel != "":
-		await client.send_message(consolechannel, content)
+		if messagetype == 1:
+			await client.send_message(consolechannel, "[CONSOLE] " + content)
+		elif messagetype == 2:
+			await client.send_message(consolechannel, "[INFO] " + content)
+		elif messagetype == 3:
+			await client.send_message(consolechannel, "[INTERNAL] " + content)
+		elif messagetype == 4:
+			await client.send_message(consolechannel, "[ALERT] " + client.user.get_user_info("174827375639396352").mention + ": " + content)
 	else:
 		print("Error: Console channel not defined!")
 	
@@ -141,16 +101,12 @@ async def on_message(message):
 		if tmp.startswith('%debugvar'):
 			tmp = tmp[10:]
 			tmp = int(tmp)
-			await print_console_channel(varlist[tmp])
+			await print_console_channel(str(varlist[tmp]), 1)
 	elif message.content.startswith('%listchannels'):
 		for channel in message.server.channels:
 			print(channel)
 	elif message.content.startswith('%channelid'):
 		await client.send_message(message.channel, message.channel.id)
-	elif message.content.startswith('%addchannel'):
-		global channels
-		channels.append(message.channel)
-		await client.send_message(message.channel, "Channel added successfully.")
 	elif message.content.startswith('%relay'):
 		tmp = str(message.content)
 		tmp = tmp[7:]
@@ -177,15 +133,66 @@ async def on_message(message):
 			updateconsoleoutputchannel()
 	elif message.content.startswith("%echo"):
 		if message.author.id != client.user.id:
-			await print_console_channel(message.content)
-	elif message.content.startswith("%genserverentry"):
-			with open(message.server.id, "w") as file:
-				tmp = message.channel
-				tmp2 = message.author
-				await client.send_message(tmp, "Set your server's relay channel using %setrelaychannel.")
-				tmp = client.wait_for_message(timeout=60, channel=message.channel, author=tmp2, content="%setrelaychannel")
-				print(message.author.server_permissions.administrator)
-				if message.author.server_permissions.administrator:
-					file.write(message.channel.id)
-					await client.send_message(message.channel, "This channel has been set as the relay channel.")
+			await print_console_channel(message.content, 1)
+	elif message.content.startswith("%gennationentry"):
+		if message.author.id in nationmods or message.author.id == "174827375639396352":
+			await client.send_message(message.channel, "Follow these instructions to generate a nationfile:" + "\n" + "Type %setnationstats with the following arguments in this order:")
+			await client.send_message(message.channel, "Name, Vassal Of, Treasury, GPT, Friendships, Open Borders, Alliances, Defensive Pacts, Denouncing, War.")
+			await client.send_message(message.channel, "If multiple names are on a line, separate them with slashes (ie Chile/Poland).")
+			tmp = await client.wait_for_message(timeout=60, channel=message.channel, author=message.author)
+			tmp = str(tmp.content)
+			tmp2 = tmp.split()
+			print(str(tmp2))
+			if tmp2[0].startswith("%setnationstats"):
+				tmp2.remove("%setnationstats")
+				tmp3 = tmp2[0]
+				tmp2 = " ".join(tmp2)
+				with open(tmp3, "w") as file:
+					tmp3 = tmp3.split()
+					print(str(tmp3))
+					tmp2 = tmp2.split()
+					file.close()
+				with open(tmp3[0], "a") as file:
+					for stat in tmp2:
+						file.write(stat + "\n")
+					file.write("&&&&&" + "\n")
+				if tmp3[0] not in nations:
+					await addnation(tmp3[0])
+				await client.send_message(message.channel, "Now, send %setlocalnationmods and @mention a list of people you want to be able to mod your nation's bot stuff.")
+				tmp = await client.wait_for_message(timeout=60, channel=message.channel, author=message.author)
+				tmp7 = tmp.raw_mentions
+				tmp = str(tmp.content)
+				tmp2 = tmp.split()
+				print(str(tmp2))
+				if tmp2[0].startswith("%setlocalnationmods"):
+					tmp2.remove("%setlocalnationmods")
+					tmp5 = " ".join(tmp7)
+					tmp5 = tmp5.split()
+					print(tmp5)
+					with open(tmp3[0], "a") as file:
+						for user in tmp5:
+							file.write(str(user))
+							file.write("\n")
+						file.write("&&&&&")
+				await client.send_message(message.channel, "Now, send %setrelaychannel in the channel you want to have incoming messages sent to. Make sure I can see it.")
+				tmp = await client.wait_for_message(timeout=60, author=message.author, content="%setrelaychannel")
+				with open(tmp3[0], "a") as file:
+					file.write("\n" + str(tmp.channel.id) + "\n")
+					file.write("&&&&&")
+				await client.send_message(message.channel, "All done!")
+	
+	elif message.content.startswith("%addnationmod"):
+		if message.author.id in nationmods or message.author.id in author or message.author.id == '174827375639396352':
+			tmp = message.raw_mentions
+			tmp = " ".join(tmp)
+			print(tmp)
+			with open('nationmods', 'a') as file:
+				file.write(tmp + '\n')
+			await client.send_message(message.channel, "Successfully added user to nation mods list.")
+			await print_console_channel("User added as nation mod.", 2)
+			updatenationmods()
+		else:
+			await client.send_message(message.channel, "You do not have permission to execute this command.")
+			await print_console_channel("Attempt at %addnationmod from" + message.raw_mentions, 4)
+	
 client.run('MjQ4MjQwMzg2ODY5Mjk3MTU1.Cw3Q_Q.AgwD4S6h3SrnDXG2H6Utrgwo11k')
